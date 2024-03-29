a1501ce1aeaf20f11f016422e6657f2594278e95
Roj <rvadiaz09@gmail.com>
2024-03-30 07:07:55 +0800

    added projects (1), shift to tailwind next

--- src/assets/json/ProjectsList.js
+++ src/assets/json/ProjectsList.js
@@ -1,6 +1,6 @@
 const ProjectsList = [
     { title: 'Web Portfolio', description: 'Showcasing knowledge in the fundamentals of web development. Made with React Showcasing knowledge in the fundamentals of web development. Made with React.', image: "/assets/images/portfolio.png", demo: 'https://rhoge-portfolio.vercel.app/', code: 'https://github.com/RhogeDiaz/vanilla-porfolio.git' },
-    { title: 'Green Bristle', description: 'A basic E-Commerce app showcasing skills in original web design, UI component layouts, and knowledge in modern designs.', image: "/assets/images/green-bristle.png", demo: '', code: '' },
+    { title: 'Eats Elite', description: 'A landing site for a meal planner app, showcasing skills in UI layouts and good coding practices. Made with React and Tailwind.', image: "/assets/images/eats-elite.png", demo: 'https://eats-elite-green.vercel.app/', code: 'https://github.com/RhogeDiaz/eats-elite.git' },
 ]
 
 export default ProjectsList

71b64150a73fe5527d0e117968364e0f7e304663
Roj <rvadiaz09@gmail.com>
2024-06-04 10:17:01 +0800

    added one-vs-ufc

--- src/assets/json/ProjectsList.js
+++ src/assets/json/ProjectsList.js
@@ -2,6 +2,7 @@ const ProjectsList = [
     { title: 'Web Portfolio', description: 'Showcasing knowledge in the fundamentals of web development. Made with React Showcasing knowledge in the fundamentals of web development. Made with React.', image: "/assets/images/portfolio.png", demo: 'https://rhoge-portfolio.vercel.app/', code: 'https://github.com/RhogeDiaz/vanilla-porfolio.git' },
     { title: 'Eats Elite', description: 'A landing site for a meal planner app, showcasing skills in UI layouts and good coding practices. Made with React and Tailwind.', image: "/assets/images/eats-elite.png", demo: 'https://eats-elite-green.vercel.app/', code: 'https://github.com/RhogeDiaz/eats-elite.git' },
     { title: 'Wordpress Site', description: 'A sample page I made using WordPress where I presented my old and new projects and my path up until now. Made with WordPress.', image: '/assets/images/wordpress-site.png', demo: 'https://rthoughts3.wordpress.com/', code: 'https://rthoughts3.wordpress.com/' },
+    { title: 'ONE vs UFC', description: 'A survey website I made to know who the majority of people think would win in a fight between UFC and ONE fighters Made with Supabase, React, and Tailwind.', image: '/assets/images/one vs ufc.png', demo: 'https://one-vs-ufc.vercel.app/', code: 'https://github.com/RhogeDiaz/one-vs-ufc' },
 ]
 
 export default ProjectsList
